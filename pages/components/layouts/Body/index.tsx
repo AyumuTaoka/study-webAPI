cabbaa6d335637daf1e9f847bf9487afcd5ab678
AyumuTaoka <a.taoka@4nonome.com>
2022-12-01 14:32:59 +0900

    searchBarのインポートの削除

--- pages/components/layouts/Body/index.tsx
+++ pages/components/layouts/Body/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import styles from "styles/Home.module.css";
 import { Todo } from "../../organisms/Todo";
-import { TopSearchBar } from "../../organisms/TopSearchBar";
 
 export function Body() {
   return (
