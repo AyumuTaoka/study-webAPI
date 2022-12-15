8e66f0f63bee4758cddc93a4c9ce6352a17f5896
AyumuTaoka <a.taoka@4nonome.com>
2022-12-15 17:11:17 +0900

    絵文字追加🐈

--- pages/components/layouts/Header/index.tsx
+++ pages/components/layouts/Header/index.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export default function Header() {
   return (
     <HeaderWrapper>
-      <NekoTitle>ねこ</NekoTitle>
+      <NekoTitle>🐈ねこ🐈‍⬛</NekoTitle>
     </HeaderWrapper>
   );
 }
